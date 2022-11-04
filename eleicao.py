41a7748be07d81928eb150356980189c3e0d9018
VitorRez <vitorez2002@gmail.com>
2022-11-03 23:08:06 -0300

    alterado o ambiente de eleição. Geração de chaves publicas

--- eleicao.py
+++ eleicao.py
@@ -1,55 +1,79 @@
 import csv
+from Crypto.PublicKey import RSA
 
+#classe para armazenar todos os dados do .csv
 class eleicao():
 
-    def __init__(self, nome, cpf, unidade, validade):
+    def __init__(self, nome, cpf, unidade, chave_pub, validade, candidato, data, horai, horaf):
         self.nome = nome
         self.cpf = cpf
         self.unidade = unidade
-        self.validade = validade
-
-
-class eleitor(eleicao):
-
-    def __init__(self, nome, cpf, unidade, chave_pub, validade, datai, dataf, horai, horaf):
-        super().__init__(nome, cpf, unidade, validade)
         self.chave_pub = chave_pub
-        self.datai = datai
-        self.dataf = dataf
+        self.validade = validade 
+        self.candidato = candidato
+        self.data = data
         self.horai = horai
         self.horaf = horaf
 
-class candidato(eleicao):
-
-    def __init__(self, nome, cpf, unidade, validade, num_votos):
-        super().__init__(nome, cpf, unidade, validade)
-        self.num_votos = num_votos
-
-def readDB_c(filename, cand_list):
+#le de um arquivo .csv e lista todas as pessoas cadastradas como candidatos
+def readDB(filename, cand_list):
 
     with open(filename, 'r') as arquivo_csv:
         leitor_csv = csv.DictReader(arquivo_csv)
 
         for linha in leitor_csv:
-            if linha['candidato'] == 1:
-                nome = linha['Nome']
-                cpf = linha['cpf']
-                unidade = linha['unidade']
-                validade = linha['unidade']
-                num_votos = linha['num_votos']
-                c = candidato(nome, cpf, unidade, validade, num_votos)
-                cand_list.append(c)
+            if linha['candidato'] == "1":
+                e = eleicao(linha['nome'], linha['cpf'], linha['unidade'], linha['chave_pub'], linha['validade'], linha['candidato'], linha['data'], linha['horai'], linha['horaf'])
+                cand_list.append(e)
 
+#altera o arquivo .csv quando algum dado for alterado (chave publica, validade, etc)
+#não consegui implementar ainda uma forma de fazer isso sem armazenar todos os dados do arquivo, logo, trecho sujeito a mudanças
+def writeDB(filename, e_list):
+    
+    with open(filename, "w+") as arquivo_csv:
+        escreve_csv = csv.writer(arquivo_csv)
+        escreve_csv.writerow(['nome','cpf','unidade','chave_pub','validade','candidato','data','horai','horaf','num_votos'])
+        for i in e_list:
+            escreve_csv.writerow([i.nome, i.cpf, i.unidade, i.chave_pub, i.validade, i.candidato, i.data, i.horai, i.horaf])
+
+#verifica se o individuo é um eleitor válido (não está votando uma segunda vez, vota naquela "unidade" e possui nome e cpf válido)
+#se o eleitor for válido, é gerada uma chave publica de RSA, exportada no formato PEM
+#ao final altera o arquivo .csv
 def validator_e(filename, nome, cpf, unidade):
 
-    with open(filename, 'r') as arquivo_csv:
+    with open(filename, "r+") as arquivo_csv:
         leitor_csv = csv.DictReader(arquivo_csv)
 
+        e = None
+        e_list = []
         for linha in leitor_csv:
-            if linha['candidato'] == 0 and linha['validade'] == 1 and linha['unidade'] == unidade:
+            if linha['candidato'] == '0' and linha['validade'] == '1' and linha['unidade'] == unidade:
                 if linha['nome'] == nome and linha['cpf'] == cpf:
-                    e = eleitor(linha['nome'], linha['cpf'], linha['unidade'], linha['chave_pub'], linha['validade'], linha['datai'], linha['dataf'], linha['horai'], linha['horaf'])
-                    return e
+                    chave = RSA.generate(2048)
+                    linha['chave_pub'] = chave.publickey().exportKey("PEM")
+                    linha['validade'] = 0
+                    e = eleicao(linha['nome'], linha['cpf'], linha['unidade'], linha['chave_pub'], linha['validade'], linha['candidato'], linha['data'], linha['horai'], linha['horaf'])
+            e_geral = eleicao(linha['nome'], linha['cpf'], linha['unidade'], linha['chave_pub'], linha['validade'], linha['candidato'], linha['data'], linha['horai'], linha['horaf'])
+            e_list.append(e_geral)
+
+    writeDB(filename, e_list)
+    return e
+
+cand_list = []
+readDB("teste.csv", cand_list)
+for i in cand_list:
+    print(i.nome)
+i = 1
+while i == 1:
+    nome = input()
+    cpf = input()
+    unidade = input()
+    e = validator_e("teste.csv", nome, cpf, unidade)
+    print(e.nome)
+    i = int(input())
+
+
+
 
 
 
